aece220e0781069c6e67183fafc02a6c5d62b5a0
gdrangel <gdrangel@asu.edu>
2025-09-26 20:54:28 -0700

    draft

---
+++ javascript.js
@@ -0,0 +1,41 @@
+function submitter(e) {
+    e.preventDefault();
+    let name = document.getElementById("name");
+    let email = document.getElementById("emailAdd");
+    let comments = document.getElementById("comments");
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+    let nameSpan = name.previousElementSibling;
+    let emailSpan = email.previousElementSibling;
+    let commentSpan = comments.previousElementSibling;
+    let result = document.getElementById("result");
+    let hasError = false;
+    nameSpan.innerHTML = "";
+    emailSpan.innerHTML = "";
+    commentSpan.innerHTML = "";
+    result.innerHTML = "";
+    if (name.value.trim().length < 1) {
+        nameSpan.innerHTML = "Please enter a valid name.";
+        hasError = true;
+    }
+    if (!emailRegex.test(email.value.trim())) {
+        emailSpan.innerHTML = "Please enter a valid email address.";
+        hasError = true;
+    }
+    if (comments.value.trim().length < 1) {
+        commentSpan.innerHTML = "Please enter a comment.";
+        hasError = true;
+    }
+    if (hasError) return;
+    const person = {
+        name: name.value.trim(),
+        email: email.value.trim(),
+        comments: comments.value.trim()
+    };
+    result.innerHTML = `You sent:<br>
+    Name: ${person.name}<br>
+    Email: ${person.email}<br>
+    Comments: ${person.comments}`;
+    document.getElementById("content").reset();
+}
+
+document.querySelector("form").addEventListener("submit", submitter);
