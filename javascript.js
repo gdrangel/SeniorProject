7a74bb0e93e970e6b10f2a93bfaf22125ead4766
gdrangel <gdrangel@asu.edu>
2025-10-09 22:04:47 -0700

    final

--- javascript.js
+++ javascript.js
@@ -1,3 +1,32 @@
+"use strict";
+
+const pageTopBtn = document.getElementById('pagetop');
+
+pageTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+});
+
+function filter() {
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById('filter');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("thelist");
+	li = ul.getElementsByTagName('li');
+	
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
+}
+
 function submitter(e) {
     e.preventDefault();
     let name = document.getElementById("name");
